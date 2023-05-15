6b013bf8be366eaa1d011a3c1b3253d63245b36a
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:20:36 +0200

    [feat]:Needed app utils

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<NavBar />
   <router-view/>
 </template>
 
@@ -24,7 +21,13 @@ nav a {
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
+}
+
+</script>
