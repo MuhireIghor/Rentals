bb238cfbfe29b7df89ac89833f69b8990bebefd2
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:17:27 +0200

    [feat]:Addition of the Layout pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<MainLayout>
+
+  <div class="pt-32 w-full bg-gray-200 relative max-h-screen">
+    <div class="w-full flex">
+    <!-- section 1 -->
+    <div class="h-[300px] ml-32 w-1/2 items-center space-y-12">
+      <p class="text-3xl text-black font-semibold text-start">
+        Looking to save more on your rental car?
+      </p>
+      <p class="text-start">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        perspiciatis aut eius accusantium possimus rerum, voluptatum vitae quis
+        nam suscipit?
+      </p>
+      <div class="flex w-full gap-12 ">
+        <ButtonComponent className="bg-white text-black flex items-center justify-center py-8 text-md font-bold" buttonText='Continue with Google Play'  />
+        <ButtonComponent className="bg-white text-black w-1/2 flex items-center justify-center py-8 text-md font-bold" buttonText='Continue with Apple Store' />
+      </div>
+    </div>
+    <div class="bg-[#b85930] h-[400px] w-1/3 mx-auto rounded-md relative">
+      <img
+        src="../assets/car.png"
+        class="w-full h-3/4 absolute -left-32 rotate-180"
+      />
+    </div>
+    </div>
+    <div class="w-2/3">
+    <SearchBar />
+    </div>
   </div>
+</MainLayout>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ButtonComponent from '../components/ButtonComponent.vue'
+import SearchBar from '../components/SearchBar.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
+    ButtonComponent,
+    SearchBar,
+    MainLayout
   }
 }
 </script>
