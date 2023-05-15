db5fdd52add7202845204680fb0361e3c47ce582
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:18:18 +0200

    [feat]:Initial app routing configurations

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignupView from '../views/auth/SignupView.vue'
+import LoginView from '../views/auth/LoginView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -14,6 +17,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/auth/signup',
+    name: 'signup',
+    component: SignupView
+  },
+  {
+    path: '/auth/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notFound ',
+    component: NotFound
   }
 ]
 
