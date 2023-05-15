3c988776cc05a92d296df12f9c01a92dea05a417
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:07:11 +0200

    [feat]:Button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :class="`rounded-xl w-1/4 h-10 my-auto hover:shadow-md ${className} `" @click="clickHandler">{{buttonText}}</button>
+</template>
+<script>
+export default {
+  data () {
+  },
+  props: {
+    buttonText: String,
+    clickHandler: Function,
+    className: String
+  }
+}
+</script>
