5c3029ca82172faade1357f91700ea613d74aed8
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:14:57 +0200

    [feat]:sign up page form implementation

---
+++ src/views/auth/SignupView.vue
@@ -0,0 +1,11 @@
+<template>
+  <FormComponent title="Sign Up" :isLogin="true"/>
+</template>
+<script>
+import FormComponent from '../../components/FormComponent.vue'
+export default {
+  components: {
+    FormComponent
+  }
+}
+</script>
