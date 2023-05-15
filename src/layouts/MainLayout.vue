05a3e91ad689d4dd80d534d87899d2dcdecfbc76
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:11:46 +0200

    [feat]:Main Layout component

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='pt-24'>
+    <NavBar />
+    <slot></slot>
+  </div>
+</template>
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
