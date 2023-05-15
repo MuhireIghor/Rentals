75ca04c56889a28362921f524f16ef5243db05c9
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:11:03 +0200

    [feat]:QuickSearch bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='bg-white rounded p-4 w-[40%] flex justify-evenly shadow-md shadow-indigo-200 z-20 absolute left-[12%] -bottom-8'>
+        <!-- choose location  -->
+        <div class='flex hover:cursor-pointer'>
+            <p>Choose a location</p>||
+            </div>
+        <div class='flex hover:cursor-pointer'>
+            <p>Pick up date</p>||
+            </div>
+        <div class='flex hover:cursor-pointer'>
+            <p>Return date</p>
+            </div>
+            <div>
+                <ButtonComponent className="flex bg-indigo-500"/>
+            </div>
+
+    </div>
+</template>
+<script>
+import ButtonComponent from './ButtonComponent.vue'
+export default {
+  components: {
+    ButtonComponent
+  }
+}
+</script>
