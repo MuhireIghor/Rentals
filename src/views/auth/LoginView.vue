facaf9ecbec7d70dfd43c4046b2c1ca9a16c08a7
MuhireIghor <muhireighor123@gmail.com>
2023-05-15 13:14:35 +0200

    [feat]:Login page form implementation

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,11 @@
+<template >
+  <FormComponent title="Log in" :isLogin="false" />
+</template>
+<script>
+import FormComponent from '../../components/FormComponent.vue'
+export default {
+  components: {
+    FormComponent
+  }
+}
+</script>
