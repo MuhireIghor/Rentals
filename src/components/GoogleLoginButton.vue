ff16a1b6a31c25239f39f1cc741f6cdf27bb1756
MuhireIghor <muhireighor123@gmail.com>
2023-07-27 15:46:48 +0200

    [feat]Added the google login custom button

---
+++ src/components/GoogleLoginButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="login" :class="`${className}`">Sign In with Google</button>
+</template>
+<script>
+export default {
+  name: 'GoogleLoginButton',
+  methods: {
+    login () {
+      this.$gAuth.signIn().then(googleUser => {
+        console.log(googleUser)
+      }).catch(err => console.log(err))
+    }
+  },
+  props: {
+    className: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
