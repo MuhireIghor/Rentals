d83334a4740c2ae6636c993361e094e4ffab9cee
MuhireIghor <muhireighor123@gmail.com>
2023-07-27 15:45:06 +0200

    [feat]Chnaged the google oauth plugin to version3

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import GAuth from 'vue3-google-oauth2'
 import './index.css'
-
-createApp(App).use(store).use(router).mount('#app')
+const gauthOption = {
+  clientId: '406745584266-r4gev3htl1fnjb4i23n2b2uraaeakiam.apps.googleusercontent.com',
+  apiKey: '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',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+createApp(App).use(GAuth, gauthOption).use(store).use(router).mount('#app')
