c8cdbad0ac634648bdc0c262a1e4e5bc0736eeb0
MuhireIghor <muhireighor123@gmail.com>
2023-07-28 16:20:40 +0200

    [update]Changed the profile pic rendering

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex justify-evenly items-baseline bg-white shadow-md shadow-indigo-200 px-12 ">
+  <div class="w-full flex justify-evenly items-baseline bg-white px-12 border-b-2 border-indigo-200">
     <div class="w-1/4 flex items-center">
      Rental X
     </div>
@@ -29,9 +29,9 @@
         <ButtonComponent buttonText='SignUp' :clickHandler="handleSignup" className="bg-indigo-500 text-white w-1/3" />
         <ButtonComponent buttonText='Log in' :clickHandler="handleLogin" className="bg-indigo-500 text-white w-1/3"/>
     </div>
-    <div class="w-1/6 shadow-md border-1 " v-else @click="handleNavigation">
-      <div class="w-12 h-12 bg-orange-500 text-white rounded-full bg-white flex items-center justify-center mx-auto" v-if="this.$store.state.loggedIn">
-  <p class="text-xl font-medium">{{ this.$store.state.user.username.slice(0,2) }}</p>
+    <div class="w-1/6 shadow-md border-1 translate-y-6 " v-else @click="handleNavigation">
+      <div class="w-20 h-12 mx-auto" v-if="this.$store.state.loggedIn">
+  <img class="text-xl font-medium rounded-full" width="20" height="20" :src="this.$store.state.user.picture" alt="profile"/>
       </div>
 <p class="text-gray-500">{{ this.$store.state.user.email }}</p>
     </div>
