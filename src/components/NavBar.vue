e142a4827bfe9d6167ee1c7fd4b296b156b51795
MuhireIghor <muhireighor123@gmail.com>
2023-07-28 13:21:48 +0200

    [feeat]User details retrieval from the google profile object

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex fixed justify-evenly bg-white shadow-md shadow-indigo-200 px-12">
+  <div class="w-full flex justify-evenly items-baseline bg-white shadow-md shadow-indigo-200 px-12 ">
     <div class="w-1/4 flex items-center">
      Rental X
     </div>
@@ -25,9 +25,15 @@
         >Products</router-link
       >
     </nav>
-    <div class='w-1/4 flex gap-6 justify-end mr-4'>
+    <div class='w-1/4 flex gap-6 justify-end mr-4' v-if="!this.$store.state.loggedIn">
         <ButtonComponent buttonText='SignUp' :clickHandler="handleSignup" className="bg-indigo-500 text-white w-1/3" />
         <ButtonComponent buttonText='Log in' :clickHandler="handleLogin" className="bg-indigo-500 text-white w-1/3"/>
+    </div>
+    <div class="w-1/6 shadow-md border-1 " v-else @click="handleNavigation">
+      <div class="w-12 h-12 bg-orange-500 text-white rounded-full bg-white flex items-center justify-center mx-auto" v-if="this.$store.state.loggedIn">
+  <p class="text-xl font-medium">{{ this.$store.state.user.username.slice(0,2) }}</p>
+      </div>
+<p class="text-gray-500">{{ this.$store.state.user.email }}</p>
     </div>
   </div>
 </template>
@@ -43,6 +49,9 @@ export default {
     },
     handleLogin () {
       this.$router.push({ name: 'login' })
+    },
+    handleNavigation () {
+      this.$router.push({ name: 'user' })
     }
   }
 }
