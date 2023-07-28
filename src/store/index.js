c4a0848c6f46e2203622738cb7de0b068948699e
MuhireIghor <muhireighor123@gmail.com>
2023-07-28 16:21:33 +0200

    [feat]Added the profile pic property

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    loggedIn: false,
+    user: {}
   },
   getters: {
+    isAuthenticated (state) {
+      return state.loggedIn
+    }
   },
   mutations: {
+    login (state, payload) {
+      state.loggedIn = true
+      state.user.username = payload.name
+      state.user.email = payload.email
+      state.user.picture = payload.picture
+    }
   },
   actions: {
   },
